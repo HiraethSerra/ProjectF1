94b69ed4a8f6b273acb4d8b755ecd7a45654fb6e
unknown <tresaserrafina@gmail.com>
2025-11-18 23:49:13 +0100

    hh

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
 import { Home } from "./pages/Home";
 import { ResultsPage } from "./pages/ResultsPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom"; 
 import { NotFound } from "./pages/NotFound";
 import Teams from "./pages/Teams";
 import Drivers from "./pages/Drivers";
-
 import './index.css';
 
 
@@ -26,31 +24,4 @@ function App() {
   );
 }
 
-=======
-import { Home } from "./pages/Home";
-import { ResultsPage } from "./pages/ResultsPage";
-import { BrowserRouter, Routes, Route } from "react-router-dom"; 
-import { NotFound } from "./pages/NotFound";
-import Teams from "./pages/Teams";
-
-import './index.css';
-
-
-function App() {
-
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-            <Route index element={<Home />}/>
-            <Route path="/results" element={<ResultsPage />} />
-            <Route path="*" element={<NotFound />}/>
-            <Route path="/teams" element={<Teams />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
-}
-
->>>>>>> 1b71780ae15c83ce0bd1286dca2135bea04afa8c
 export default App;
